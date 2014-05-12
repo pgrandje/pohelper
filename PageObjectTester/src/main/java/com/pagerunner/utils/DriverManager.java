d6a4c6923a84f3b226b126f296d79295697e1216
Paul Grandjean <pgrandje@gmail.com>
2014-05-11 18:51:47 -0700

    Optimized imports

--- PageObjectTester/src/main/java/com/pagerunner/utils/DriverManager.java
+++ PageObjectTester/src/main/java/com/pagerunner/utils/DriverManager.java
@@ -4,11 +4,6 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 
 /**
