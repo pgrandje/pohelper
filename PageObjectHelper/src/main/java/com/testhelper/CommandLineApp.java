8ae7cdfa634a16a526d716a71d08edbe5d9c6909
Paul Grandjean <pgrandje@gmail.com>
2015-02-01 20:08:31 -0800

    And one more TODO on using Command Message object

--- PageObjectHelper/src/main/java/com/testhelper/CommandLineApp.java
+++ PageObjectHelper/src/main/java/com/testhelper/CommandLineApp.java
@@ -27,6 +27,7 @@ public class CommandLineApp {
 
         // TODO:  See if there's some way to keep the command-line processing completely within the Command-line processor to avoid having to come back to main-app.
         // todo:  Perhaps it's best just to move the whole interaction between Command-line Processor and Generator into the Command-line Processor?
+        // TODO: Passing a Command Message object would decouple the generator from the interface.
         if (getCommandLineProcessor().getGenerateType() == Generator.GenerateType.INTERACTIVE) {
             getCommandLineProcessor().runInteractiveMode();
         }
